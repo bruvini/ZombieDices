82f0e8b054174e55200d22054ff2f76a0a26bc31
bruvini <86611700+bruvini@users.noreply.github.com>
2022-03-03 21:54:11 -0300

    v1.4
    Atualização de UI
    Correção da contagem de jogadores
    Inicio do código para execução das rodadas

--- prototipo inicial.py
+++ prototipo inicial.py
@@ -23,27 +23,53 @@ contagem_tiros = 0 #quantidade de tiros na rodada
 contagem_cerebros = 0 #quantidade de cerebros na rodada
 
 #Mensagem de saudação aos jogadores
+print (100*'-')
 print("Olá, ZUMBIIIIIS! Sejam bem-vindos ao Zombie Dices, um jogo onde o melhor comedor de cérebros VENCE!")
 time.sleep(1) #Pausa de 1 segundo até o próximo comando
+print (100*'-')
+
+print ()
 
 #Informar a quantidade de jogadores e analisar se tem a quantidade mínima necessária
 print("Para tudo funcionar, precisamos de pelo menos dois zumbis famintos por cérebros...")
-print("Para finalizar o registro de jogadores, basta apertar ENTER para enviar a resposta em branco")
-nome = str(input("Digite o nome do jogador {}: ".format((qtd_jogadores))))
+print("Escreva o nome dos jogadores em cada linha, e quando não tiver mais, basta apertar ENTER para enviar a resposta em branco")
 
+print ()
+
+print (100*'-')
+nome = str(input("Digite o nome do jogador 1: ")) #armazena o nome dos jogadores
+
+#Estrutura para armazenar o nome dos jogadores em uma lista assim como atualizar a quantidade de jogadores
 while nome !=  '':
-    players.append(nome)
-    qtd_jogadores += 1
-    nome = str(input("Digite o nome do jogador {}: ".format((qtd_jogadores))))
+    players.append(nome) #armazena o nome dos jogadores na lista PLAYERS
+    qtd_jogadores += 1 #armazena a quantidade de jogadores
+    nome = str(input("Digite o nome do jogador {}: ".format((qtd_jogadores)))) #gera input para a inserção de outro jogador
+    if nome == '':
+        qtd_jogadores -= 1 #se o output for vazio, não irá contabilizar no número total de jogadores
 
+print ()
+
+#identifica se há jogadores em quantidade suficiente
 if qtd_jogadores <2:
-    print("É necessário 2 ou mais zumbis para o jogo começar! Reinicie o Jogo!")
+    print("É necessário 2 ou mais zumbis para o jogo começar! Reinicie o Jogo!") #fecha se tiver menos que 2 jogadores
 else:
-    print("Vamos começar a caça aos cérebros?")
+    print("Vamos começar a caça aos cérebros?") #continua para a rodada se tiver 2 ou mais jogadores
+print (100*'-')
 
+print ()
+
+time.sleep(1)
+
+print (100*'-')
+#executa as ações de cada rodada para cada jogador na lista PLAYERS
 for i in players:
-    play = str(input("{} é sua vez de jogar. Aperte enter para iniciar: ".format(i)))
-
+    play = str(input("{} é sua vez de jogar. Digite S para continuar e N para finalizar: ".format(i)))
+    play_min = play.lower()
+    if 's' or 'n' in play_min:
+        if play_min == "n":
+           print("Ok! Vai perder de comer alguns cérebros, {}...".format(i))
+        else:
+            print ("ok")
 
 
 
